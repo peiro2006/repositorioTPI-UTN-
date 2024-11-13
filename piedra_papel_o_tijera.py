cfe5c8b84b7a197fc2d8ef2201a10725e93ea259
MatiasMedina846 <matimedina111@gmail.com>
2024-11-12 21:59:21 -0300

    Add files via upload

--- piedra_papel_o_tijera.py
+++ piedra_papel_o_tijera.py
@@ -1,10 +1,10 @@
-def piedrapapeltijera(ejemplo):
-    import random
+def piedrapapeltijera(ejemplo): #Aplicamos la función 
+    import random #Le pedimos a la maquina que elija entre piedra, papel o tijeras de forma aleatoria
     print("")
     print("Piedra = 1")
     print("Papel = 2        ¡Gana el mejor de 3!")
     print("Tijera = 3")
-    print("")
+    print("") #Tomamos la decision entre piedra, papel o tijeras
 
     jugando= 0 
     rM = 0
@@ -12,15 +12,15 @@ def piedrapapeltijera(ejemplo):
 
     juego = [1,2,3]
 
-    while rM != 3 and rJ != 3:
+    while rM != 3 and rJ != 3: 
         eleccionMaquina = random.sample(juego,1)[0]
-        eleccion = int(input("Eleji entre las 3: "))
+        eleccion = int(input("Eleji entre las 3: ")) #Aquí la maquina elige primero, y luego seleccionamos una opción nosotros
 
         if eleccion == eleccionMaquina:
-            print("Empate")
+            print("Empate") 
 
-        if eleccion == 1 and eleccionMaquina == 3:
-            print("La maquina eligió tijera, ganaste!")
+        if eleccion == 1 and eleccionMaquina == 3: 
+            print("La maquina eligió tijera, ganaste!") 
             rJ = rJ + 1
         else:
             if eleccion == 1 and eleccionMaquina == 2:
@@ -45,7 +45,7 @@ def piedrapapeltijera(ejemplo):
                 print("La maquina eligió piedra, perdiste!")
                 rM = rM + 1
 
-    while jugando == 0:
+    while jugando == 0: #Llegados a este punto, se decidirá quien gana o pierde
         if rJ == 3:
             print("¡Ganaste!")
             break
@@ -53,3 +53,6 @@ def piedrapapeltijera(ejemplo):
             if rM == 3:
                 print("¡Perdiste!")
                 break
+ejemplo = 0
+piedrapapeltijera(ejemplo)
+
